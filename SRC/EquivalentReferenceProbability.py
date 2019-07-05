d658c1c49353d37ad3f70d7b5c7c901969e01326
tais <tgrippa@ulb.ac.be>
2019-07-05 11:34:03 +0200

    improve readability of the code

--- SRC/EquivalentReferenceProbability.py
+++ SRC/EquivalentReferenceProbability.py
@@ -7,13 +7,13 @@ This function is used to compute ERP (Equivalent Reference Probability) from a c
 
 import csv, os, sys, math, tempfile
 
-def ComputeERPfromCsv(in_path, delimiter=',', erp_name="ERP", start_index=1):   # "start_index" should contain the index where probabilities starts, "stop_index" where it stops.
+def ComputeERPfromCsv(in_path, delimiter=',', erp_name="ERP", start_index=1, stop_index=False):   # "start_index" should contain the index where probabilities starts, "stop_index" where it stops.
     # Open files and create reader
     infile = open(in_path, 'r')
-    creader = csv.reader(infile, delimiter=delimiter)
+    reader = csv.reader(infile, delimiter=delimiter)
 
     # Csv header
-    out_header = creader.next() #Get the header (first line) of input csv
+    out_header = reader.next() #Get the header (first line) of input csv
     out_header.append(erp_name)
 
     # Create list containing output content
@@ -21,9 +21,12 @@ def ComputeERPfromCsv(in_path, delimiter=',', erp_name="ERP", start_index=1):   
     out_content.append(out_header) #Add the output header
 
     # Compute ERP values from probabilities and store new lines in a list
-    for row in creader:
+    for row in reader:
         out_line = row  # Copy the input file line
-        frow = [float(x) for x in row[start_index:]] #Make sure all columns are float except for Xth first column (parameter "start_index")
+        if stop_index:
+            frow = [float(x) for x in row[start_index:stop_index+1]] #Make sure all columns are float except for Xth first column (parameter "start_index")
+        else:
+            frow = [float(x) for x in row[start_index:]] #Make sure all columns are float except for Xth first column (parameter "start_index")
         probs = [x/sum(frow) for x in frow] #Make sure that the sum of probabilities equal to 1
         maxpistar = max(probs) #Take the maximum probability
         temp = [x for x in probs if x!=maxpistar] #Take probability of all but the class with the max probability
@@ -38,8 +41,8 @@ def ComputeERPfromCsv(in_path, delimiter=',', erp_name="ERP", start_index=1):   
     # Create output file
     out_path = "%s_ERP.csv"%os.path.splitext(in_path)[0]
     outfile = open(out_path, 'w')
-    cwriter = csv.writer(outfile, delimiter=delimiter)
-    cwriter.writerows(out_content)
+    writer = csv.writer(outfile, delimiter=delimiter)
+    writer.writerows(out_content)
 
     # Return
     return out_path
